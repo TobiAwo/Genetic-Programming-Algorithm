c41eb379080e71fc90481c1dbe4f255dc2c11379
unknown <tawoponle@gmail.com>
2017-11-16 21:45:25 +0000

    Adding & Evaluating Phenotype

--- src/Algorithm/main.java
+++ src/Algorithm/main.java
@@ -4,45 +4,47 @@ import java.util.Arrays;
 
 public class main {
 
-	private static int rows = 100;
-	private static int columns = 20;
+	private static int rows = 10;
+	private static int columns = 10;
 	static int[][] matrix = new int[rows][columns];
 	static int[][] matrixNext = new int[rows][columns];
-	static int GEN = 100;
+	static int GEN = 1;
 
 	public static void main(String[] args) {
 
 		System.out.println("Number of Rows: " + rows);
 		System.out.println("Number of Columns: " + columns + "\n");
 
-//		INITALIZE FIRST POPULATION
+		// INITALIZE FIRST POPULATION
 		fillMatrix(matrix);
 		System.out.println("---------------- \t Initial Popluation \t ----------------\n");
 		printMatrix(matrix);
 		System.out.println();
-//		EVALUATE FITNESS
+		// EVALUATE FITNESS
 		evaluateOverallFitness(matrix);
+		// EVALUATE PHENOTYPE FITNESS
+		countRowsFitnessPhenotype(matrix);
 		System.out.println();
 
 		for (int g = 0; g < GEN; g++) {
 
 			System.out.println("---------------- \t New Generation \t ----------------");
 
-//			EVALUATE FITNESS
-//			TOURNAMENT SELECTION
+			// EVALUATE FITNESS
+			// TOURNAMENT SELECTION
 			for (int i = 0; i < rows; i++) {
-				
-				fillMatrixNext(matrix, matrixNext, evaluateRows(matrix, (FindSumforRow(matrix, getRandomRow())),
-						(FindSumforRow(matrix, getRandomRow()))), i);
+
+				fillMatrixNext(matrix, matrixNext, evaluateRowsFitness(matrix,
+						(countRowsFitness(matrix, getRandomRow())), (countRowsFitness(matrix, getRandomRow()))), i);
 			}
 
-//			MUTATION
+			// MUTATION
 			mutation(matrixNext, getRandomRow(), getRandomColumn());
 
-//			CROSSOVER
+			// CROSSOVER
 			crossover(matrixNext, getRandomRow(), getRandomRow());
 
-//			UPDATE POPULATION
+			// UPDATE POPULATION
 			for (int j = 0; j < rows; j++) {
 				for (int k = 0; k < columns; k++) {
 					matrix[j][k] = matrixNext[j][k];
@@ -53,8 +55,6 @@ public class main {
 		printMatrix(matrix);
 		System.out.println();
 		evaluateOverallFitness(matrix);
-		sum3RandomRows(matrix, getRandomRow(), getRandomRow(), getRandomRow());
-
 	}
 
 	public static int getRandomFitness() {
@@ -101,18 +101,16 @@ public class main {
 		}
 	}
 
-	public static int[] FindSumforRow(int matrix[][], int rowIndex) {
+	public static int[] countRowsFitness(int matrix[][], int rowIndex) {
 		int sum = 0;
 		int[] row = matrix[rowIndex];
 		for (int value : row) {
 			sum = sum + value;
 		}
-		// number zero
 		if (sum == 0) {
-			
+
 			sum = 0;
 		}
-		//End
 		System.out.println("\nThe Sum of value in row " + (rowIndex + 1) + ": is " + sum);
 
 		int ar[] = new int[2];
@@ -122,29 +120,19 @@ public class main {
 		return ar;
 	}
 
-	public static int evaluateRows(int[][] matrix, int[] sumIn1, int[] sumIn2) {
+	public static int evaluateRowsFitness(int[][] matrix, int[] sumIn1, int[] sumIn2) {
 
 		int biggestRowSumIndex = 0;
 		if ((sumIn1[0]) > (sumIn2[0])) {
 
-//			if (sumIn1[0] == 0) {
-//
-//				sumIn1[0] = columns + 10;
-//			}
-
-			System.out.println("Row: " + (sumIn1[1] + 1) + " contains greater Fitness of: " + (sumIn1[0]) + "\n");
+			System.out.println("\n" + "Row: " + (sumIn1[1] + 1) + " contains greater Fitness of: " + (sumIn1[0]));
 			biggestRowSumIndex = sumIn1[1];
 		} else if ((sumIn1[0]) < (sumIn2[0]) || ((sumIn1[0]) == (sumIn2[0]))) {
 
-//			if (sumIn2[0] == 0) {
-//
-//				sumIn2[0] = columns + 10;
-//			}
-
 			biggestRowSumIndex = sumIn2[1];
 
-			System.out.println("Row: " + (sumIn2[1] + 1) + " contains greater/equal Fitness of: " + (sumIn2[0]) + "\n");
-		} 
+			System.out.println("\n" + "Row: " + (sumIn2[1] + 1) + " contains greater/equal Fitness of: " + (sumIn2[0]));
+		}
 
 		return biggestRowSumIndex;
 
@@ -164,13 +152,13 @@ public class main {
 
 			if (currentSum > maxSum) {
 				maxSum = currentSum;
-			}		
+			}
 			// number zero
 			if (i == 0) {
-				
+
 				i = 0;
 			}
-			//End
+			// End
 			System.out.println("Fitness of Creature " + (i + 1) + ": " + sum);
 		}
 		System.out.println();
@@ -179,9 +167,6 @@ public class main {
 	}
 
 	public static void mutation(int[][] matrix, int ranRow, int ranCol) {
-
-		// System.out.println("ROW : " + (ranRow + 1));
-		// System.out.println("COLUMN: " + (ranCol + 1));
 
 		if ((matrix[ranRow][ranCol]) == 0) {
 
@@ -203,38 +188,83 @@ public class main {
 				tempMatrix[row][col] = matrixCopy[row][col];
 			}
 		}
-		// System.out.println("Random row 1: " + ranRow1);
-		// System.out.println("Random row 2: " + ranRow2);
 		System.arraycopy(tempMatrix[ranRow1], 0, matrixCopy[ranRow2], 0, (matrixCopy[0].length / 2));
 		System.arraycopy(tempMatrix[ranRow2], 0, matrixCopy[ranRow1], 0, (matrixCopy[0].length / 2));
 	}
-	
-	public static void sum3RandomRows(int[][] matrixCopy, int ranRow1, int ranRow2, int ranRow3) {
-//	public static void sum3RandomRows(int[][] matrixCopy) {
 
-//		int ranRow1 = 1;
-//		int ranRow2 = 1;
-//		int ranRow3 = 2;
+	public static void countRowsFitnessPhenotype(int[][] matrixCopy) {
+		int k = 3;
+		int count = 0, countPlus = 0, countSame = 0;
 		
-		int sum = 0, sum2 = 0, sum3 = 0, sumTotal = 0;
-		int[] row = matrix[ranRow1];
-		for (int value : row) {
-			sum = sum + value;
-		}
-		
-		int[] row2 = matrix[ranRow2];
-		for (int value2 : row2) {
-			sum2 = sum2 + value2;
-		}
-		
-		int[] row3 = matrix[ranRow3];
-		for (int value3 : row3) {
-			sum3 = sum3 + value3;
-		}
+		int[][] tempMatrix = new int[rows][k];
 
-		sumTotal = sum2 + sum3 + sum;
-		System.out.println("\nThe Sum of rows " + (ranRow1 + 1) + " + " + (ranRow2 + 1) + " + " + (ranRow3 + 1) + ": are " + sumTotal);
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < (k-1); col++) {
+				for (int x = 0; x < rows; x++) {
 
-	
+					 System.arraycopy(matrixCopy[x], count, tempMatrix[x], 0, k);
+					 //countSame++;
+			
+					 }
+			}
+
+		}
+		System.out.println("\nPhenotype Fitness");
+		printMatrix(tempMatrix);	
+
 	}
+
+	public static void countRowsFitnessPhenotypDDDDDDDDDDDDDDDDDe(int[][] matrixCopy) {
+		int k = 3;
+		int maxSum = 0;
+		int sum = 0;
+		System.out.println("\n");
+		for (int i = 0; i < matrix.length; i++) {
+			sum = 0;
+			int currentSum = 0;
+			for (int j = 0; j < k; j++) {
+
+				sum += matrix[i][j];
+				currentSum += matrix[i][j];
+			}
+
+			if (currentSum > maxSum) {
+				maxSum = currentSum;
+			}
+
+			System.out.println("Phenotype Fitness of Creature " + (i + 1) + ": " + sum);
+		}
+		System.out.println();
+		System.out.println("Highest Phenotype Fitness: " + maxSum);
+
+		////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+		int[][] tempMatrix = new int[k][columns];
+
+		for (int i = 0; i < rows; i++) {
+			sum = 0;
+			int currentSum = 0;
+			for (int j = 0; j < rows; j++) {
+
+				System.arraycopy(matrixCopy[i], 0, tempMatrix[i], 0, (rows));
+
+			}
+
+			// printMatrix(tempMatrix);
+
+			///////////////////////////////////////////////////////////////////
+			// int[][] tempMatrix = new int[k][columns];
+
+			for (int row = 0; row < rows; row++) {
+				for (int col = 0; col < k; col++) {
+
+					tempMatrix[row][k] = matrixCopy[row][k];
+				}
+			}
+			// System.arraycopy(tempMatrix[ranRow1], 0, matrixCopy[ranRow2], 0,
+			// (matrixCopy[0].length / 2));
+
+			////
+		}
+	}
+
 }
